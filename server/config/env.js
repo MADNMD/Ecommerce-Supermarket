bfdaaf2ae351eb23b6d89da862e06a9db1d3c143
Mihail Donchev <mihaildonchev88@abv.bg>
2024-08-13 19:25:10 +0300

    Create env.js

---
+++ server/config/env.js
@@ -0,0 +1,5 @@
+exports.PORT = 3000;
+exports.DB_CONCETION_URL = 'mongodb://localhost:27017/vanimi-supermarket';
+exports.SECRET = 'djas-jio2323-9887dasn23f321-dsdas';
+exports.COOKIE_SESION_NAME = 'authToken';
+exports.SALT_ROUNDS = 10;
