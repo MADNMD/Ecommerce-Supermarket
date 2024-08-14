2de52476765fe4ef1f621e699ebd4b872a03feee
Mihail Donchev <mihaildonchev88@abv.bg>
2024-08-15 00:48:27 +0300

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -7,7 +7,8 @@ const fruitAndVegController = require('./controllers/fruitAndVegController');
 const getAllMilkAndEggController = require('./controllers/milkAndEggController');
 const meatAndFishController = require('./controllers/meatAndFishController');
 const breadAndPastaController = require('./controllers/breadAndPastaController');
-const allSweetAndSalty = require('./controllers/sweetAndSaltyController');
+const allSweetAndSaltyController = require('./controllers/sweetAndSaltyController');
+const drinkAndWaterController = require('./controllers/drinkAndWaterController');
 
 router.use('/users', userController);
 router.use('/email', emailController);
@@ -16,6 +17,7 @@ router.use('/fruit-and-veg-products', fruitAndVegController);
 router.use('/milk-and-egg-products', getAllMilkAndEggController);
 router.use('/meat-and-fish-products', meatAndFishController);
 router.use('/bread-and-pasta-products', breadAndPastaController);
-router.use('/sweet-and-salty', allSweetAndSalty);
+router.use('/sweet-and-salty', allSweetAndSaltyController);
+router.use('/drink-and-water', drinkAndWaterController);
 
 module.exports = router;
