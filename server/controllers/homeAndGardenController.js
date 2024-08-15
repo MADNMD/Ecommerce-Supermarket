f6733b0b0858ad2511b74194591304796d91ed93
Mihail Donchev <mihaildonchev88@abv.bg>
2024-08-16 00:11:23 +0300

    Create homeAndGardenController.js

---
+++ server/controllers/homeAndGardenController.js
@@ -0,0 +1,124 @@
+const router = require('express').Router();
+
+const productService = require('../services/productService');
+
+router.get('/get-all-home-and-garden-products', async (req, res) => {
+
+    try {
+
+        const products = await productService.getAllHomeAndGardenProducts();
+
+        res.status(200).json(products);
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+        console.log('getAllHomeAndGardenProducts-error-server');
+    }
+
+});
+
+router.get('/get-all-laundry-detergents', async (req, res) => {
+
+    try {
+
+        const products = await productService.getAllLaundryDetergents();
+
+        res.status(200).json(products);
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+        console.log('getAllLaundryDetergents-error-server');
+    }
+
+});
+
+router.get('/get-all-cleaninig-preparations', async (req, res) => {
+
+    try {
+
+        const products = await productService.getAllCleaningPreparations();
+
+        res.status(200).json(products);
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+        console.log('getAllCleaningPreparations-error-server');
+    }
+
+});
+
+router.get('/get-all-papers-napkins-foils-envelopes', async (req, res) => {
+
+    try {
+
+        const products = await productService.getAllPapersNapkinsFoilEnvelopes();
+
+        res.status(200).json(products);
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+        console.log('getAllPapersNapkinsFoilEnvelopes-error-server');
+    }
+
+});
+
+router.get('/get-all-cleaning-products', async (req, res) => {
+
+    try {
+
+        const products = await productService.getAllCleaningProducts();
+
+        res.status(200).json(products);
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+        console.log('getAllCleaningProducts-error-server');
+    }
+
+});
+
+router.get('/get-All-air-fresheners-candles-insecticides', async (req, res) => {
+
+    try {
+
+        const products = await productService.getAllAirFreshenersCandlesInsecticides();
+
+        res.status(200).json(products);
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+        console.log('getAllAirFreshenersCandlesInsecticides-error-server');
+    }
+
+});
+
+router.get('/get-only-home-and-garden', async (req, res) => {
+
+    try {
+
+        const products = await productService.getOnlyHomeAndGarden();
+
+        res.status(200).json(products);
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+        console.log('getOnlyHomeAndGarden-error-server');
+    }
+
+});
+
+module.exports = router;
