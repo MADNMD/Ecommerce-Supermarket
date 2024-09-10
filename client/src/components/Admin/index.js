aa54ee8b24bbd4487c6939ecbbd71dca660d7c1c
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-11 00:49:56 +0300

    Create index.js

---
+++ client/src/components/Admin/index.js
@@ -0,0 +1,5 @@
+export { AdminAddProduct } from './Admin-AddProduct';
+export { AdminInvetory } from './Admin-Inventory';
+export { AdminOrders } from './Admin-Orders';
+export { AdminPanel } from './Admin-Panel';
+export { AdminUsers } from './Admin-Users';
