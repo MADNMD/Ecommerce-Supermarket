1e418ac7d36053cbc763e0a4febb633073428cfc
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-11 00:50:14 +0300

    Create index.js

---
+++ client/src/components/Footer-Info/Interesting/Recipes/index.js
@@ -0,0 +1,6 @@
+export { DessertsRecipies } from './Desserts';
+export { LeanMealRecipies } from './LeanMeals';
+export { MeatDishesRecipies } from './MeatDishes';
+export { SaladRecipies } from './Salads';
+export { SmoothRecipies } from './Smoothies';
+export { SoupRecipies } from './Soups';
