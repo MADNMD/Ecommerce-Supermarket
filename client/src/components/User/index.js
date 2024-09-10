616e9d5ad2aae3f49f816c5c7f385ec619a216c0
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-11 00:50:25 +0300

    Create index.js

---
+++ client/src/components/User/index.js
@@ -0,0 +1,6 @@
+export { Favorite } from './Favorite';
+export { Profile } from './Profile';
+export { Logout } from './Logout';
+export { Login } from './Login';
+export { Register } from './Register';
+export { Order } from './Order';
