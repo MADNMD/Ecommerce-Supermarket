79511f9e86dae53c320032f67eae4fd75ac8d5f2
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-10 17:24:23 +0300

    Create index.js

---
+++ client/src/components/Category-1/index.js
@@ -0,0 +1,8 @@
+export { Fruit } from './Fruit';
+export { FruitAndVeg } from './FruitAndVeg';
+export { Salad } from './Salad';
+export { Spices } from './Spices';
+export { Vegitables } from './Vegitables';
+export { Olives } from './Olives';
+export { Dried } from './Dried';
+export { Nuts } from './Nuts';
