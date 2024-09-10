5b049291c9ba518d4dbc12fd52899b0876458cea
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-11 00:50:02 +0300

    Create index.js

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,4 @@
+export { MyCart } from './MyCart';
+export { MyCartOrder } from './MyCartOrder';
+export { MyCartReadyOrder } from './MyCartReadyOrder';
+export { MyOrderNumber } from './MyOrderNumber';
