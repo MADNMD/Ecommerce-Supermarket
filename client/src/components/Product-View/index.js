396e2c01a5c22a990b6321b7dcf42ded121d58cb
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-11 00:50:18 +0300

    Create index.js

---
+++ client/src/components/Product-View/index.js
@@ -0,0 +1,4 @@
+export { ProductInfo } from './ProductInfo';
+export { ProductInfoDiscount } from './ProductInfoDiscount';
+export { ProductInfoExhausted } from './ProductInfoExhaused';
+export { ProductInfoSecond } from './ProductInfoSecond';
