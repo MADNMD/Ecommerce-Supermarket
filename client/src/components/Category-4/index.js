88ed5da5b9befe21bea186074d7b2753af67c034
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-10 17:25:24 +0300

    Create index.js

---
+++ client/src/components/Category-4/index.js
@@ -0,0 +1,6 @@
+export {BaguettesAndTortillas} from './BaguettesAndTortillas';
+export {Bread} from './Bread';
+export {BreadAndPasta} from './BreadAndPasta';
+export {DoughProducts} from './DoughProducts';
+export {FreshPasta} from './FreshPasta';
+export {PastaProducts} from './PastaProducts';
