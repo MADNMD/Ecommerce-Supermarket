e53e372af00aef9df980757c842d3082f707b0be
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-10 17:45:55 +0300

    Create index.js

---
+++ client/src/components/Category-5/index.js
@@ -0,0 +1,5 @@
+export {BreakfastCerealsCornflakesAndMuesli} from './BreakfastCerealsCornflakesAndMuesli';
+export {ChipsAndSnacks} from './ChipsAndSnacks';
+export {SaltyProducts} from './SaltyProducts';
+export {SweetAndSalty} from './SweetAndSalty';
+export {SugarProducts} from './SugarProducts';
