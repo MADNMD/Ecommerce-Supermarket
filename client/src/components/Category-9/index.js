4852bc58f50bca859a9297b172c61273886329ea
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-10 17:46:15 +0300

    Create index.js

---
+++ client/src/components/Category-9/index.js
@@ -0,0 +1,5 @@
+export {Accessories} from './Accessories';
+export {CatFood} from './CatFood';
+export {DogAndCat} from './DogAndCat';
+export {DogFood} from './DogFood';
+export {Treats} from './Treats';
