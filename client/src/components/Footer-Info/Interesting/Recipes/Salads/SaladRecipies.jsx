513c5535a82ea54dbd9052e36895c8bcf37cd7fc
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-10 15:49:46 +0300

    Update SaladRecipies.jsx

--- client/src/components/Footer-Info/Interesting/Recipes/Salads/SaladRecipies.jsx
+++ client/src/components/Footer-Info/Interesting/Recipes/Salads/SaladRecipies.jsx
@@ -1,98 +1,14 @@
-import React, { useEffect, useState } from "react";
-import styles from '../Recipies.module.css';
-import { GoChevronLeft, GoChevronRight } from "react-icons/go";
-
-import saladsRecipies from './recipies.json'
-import { RecipieCards } from '../RecipieCards';
+import React from "react";
+import saladsRecipies from './recipies.json';
+import { RecipiesList } from '../RecipiesList';
 
 export const SaladRecipies = ({ showNavigationAndFooter }) => {
-
-    const [liked, setLiked] = useState({});
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 9;
-
-    useEffect(() => {
-        showNavigationAndFooter();
-        const savedLikes = JSON.parse(localStorage.getItem("likedSaladRecipes"));
-        if (savedLikes) {
-            setLiked(savedLikes);
-        }
-    }, [showNavigationAndFooter]);
-
-    const toggleLike = (id) => {
-        setLiked(prevLikedStates => {
-            const newLikedStates = {
-                ...prevLikedStates,
-                [id]: !prevLikedStates[id]
-            };
-            localStorage.setItem("likedSaladRecipes", JSON.stringify(newLikedStates));
-            return newLikedStates;
-        });
-    }
-
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = saladsRecipies.slice(indexOfFirstItem, indexOfLastItem);
-
-    const nextPage = () => {
-        if (currentPage < Math.ceil(saladsRecipies.length / itemsPerPage)) {
-            setCurrentPage(currentPage + 1);
-        }
-    }
-
-    const prevPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    }
-
-    const goToPage = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    }
-
-    const renderPageNumbers = () => {
-        const totalPages = Math.ceil(saladsRecipies.length / itemsPerPage);
-        const pageNumbers = [];
-
-        for (let i = 1; i <= totalPages; i++) {
-            pageNumbers.push(
-                <button
-                    key={i}
-                    onClick={() => goToPage(i)}
-                    className={i === currentPage ? styles.activePage : styles.currentPage}
-                >
-                    {i}
-                </button>
-            );
-        }
-
-        return pageNumbers;
-    }
-
     return (
-        <div className={styles['recipies-container']}>
-            <h2>Рецепти за вкусни салати</h2>
-            <div className={styles['recipes-cards']}>
-
-                {currentItems.map(card => (
-                    <RecipieCards
-                        key={card.id}
-                        id={card.id}
-                        image={card.image}
-                        title={card.title}
-                        cookTime={card.cookTime}
-                        category={card.category}
-                        liked={liked[card.id]}
-                        toggleLike={() => toggleLike(card.id)}
-                    />
-                ))}
-
-            </div>
-            <div className={styles.pagination}>
-                <button className={styles.prev} onClick={prevPage} disabled={currentPage === 1}><GoChevronLeft /></button>
-                {renderPageNumbers()}
-                <button className={styles.next} onClick={nextPage} disabled={currentPage === Math.ceil(saladsRecipies.length / itemsPerPage)}><GoChevronRight /></button>
-            </div>
-        </div>
-    )
-}
+        <RecipiesList
+            recipies={saladsRecipies}
+            likedStorageKey="likedSaladRecipes"
+            title="Рецепти за вкусни салати"
+            showNavigationAndFooter={showNavigationAndFooter}
+        />
+    );
+};
