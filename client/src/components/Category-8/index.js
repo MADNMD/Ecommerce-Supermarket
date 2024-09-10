a638936b3ce9ca9bf360650b4d8c1b1a32de95b6
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-10 17:46:11 +0300

    Create index.js

---
+++ client/src/components/Category-8/index.js
@@ -0,0 +1,7 @@
+export {CleaningPreparaions} from './CleaningPreparations';
+export {ForHomeAndGarden} from './ForHomeAndGarden';
+export {LaundryDetergents} from './LaundryDetergents';
+export {PapersAndNapkins} from './PapersAndNapkins';
+export {CleaningProducts} from './CleaningProducts';
+export {Flavorings} from './Flavorings';
+export {ForHome} from './ForHome';
