cf430ff487989702ffa9ab987ad6104eb29ce5a0
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-11 00:50:08 +0300

    Create index.js

---
+++ client/src/components/Footer-Info/index.js
@@ -0,0 +1,6 @@
+export { Contacts } from './Contacts';
+export { Cookies } from './Cookies';
+export { Delivery } from './Delivery';
+export { PricePolitic } from './PricePolitic';
+export { Holiday } from './Holiday';
+export { Interesting } from './Interesting';
