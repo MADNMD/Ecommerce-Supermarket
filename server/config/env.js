e5d9c7fee29223652c40200c13ec00778dfa768f
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-30 13:50:26 +0300

    Update env.js

--- server/config/env.js
+++ server/config/env.js
@@ -1,5 +1,6 @@
 exports.PORT = 3000;
-exports.DB_CONCETION_URL = 'mongodb://localhost:27017/vanimi-supermarket';
+exports.DB_CONECTION_URL = 'mongodb+srv://mihaildonchev88:mQvMqaSVOqXR8oDb@dev-app-cluster.i86lglg.mongodb.net/vanimi-supermarket?retryWrites=true&w=majority';
+// exports.DB_CONCETION_URL = 'mongodb://localhost:27017/vanimi-supermarket';
 exports.SECRET = 'djas-jio2323-9887dasn23f321-dsdas';
 exports.COOKIE_SESION_NAME = 'authToken';
 exports.SALT_ROUNDS = 10;
