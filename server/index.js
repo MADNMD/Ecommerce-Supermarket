4bf47f932e3c5a0f258614bd9354742e6c2e325d
Mihail Donchev <mihaildonchev88@abv.bg>
2024-09-30 13:50:32 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ const router = require('./routes');
 const app = express();
 
 expressConfig(app)
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: 'https://vanimi-supermarket.netlify.app', credentials: true }))
+// app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 app.use(cookieParser());
 app.use(router);
 
