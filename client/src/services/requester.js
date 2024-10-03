529768979ceb1b40ef7b963437b02ef3dfba0f43
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-03 13:33:04 +0300

    Update requester.js

--- client/src/services/requester.js
+++ client/src/services/requester.js
@@ -4,51 +4,26 @@ const baseURL = 'https://vanimi-supermarket.onrender.com';
 async function request(url, option) {
 
     try {
-        const response = await fetch(RS + e, t);
+
+        const response = await fetch(baseURL + url, option);
         
-        // Ако отговорът е успешен, върни го
-        if (response.ok) {
-            const contentType = response.headers.get("content-type");
-    
-            // Проверяваме дали типът на съдържанието е JSON и го обработваме
-            if (contentType && contentType.includes("application/json")) {
-                const data = await response.json();
-                return data;
-            } else {
-                const textData = await response.text();
-                return textData; // Може да върне текст, ако не е JSON
-            }
-        } else {
-            // Ако не е успешен, върни грешката като текст или JSON, ако има такъв
-            const errorMessage = await response.text();
-            throw new Error(errorMessage || "Грешка при изпращане на заявката");
+        if (response.ok !== true) {
+            const err = await response.json();
+            // throw new Error(err.message);
         }
+
+        if (response.status === 204) {
+            return response;
+        } else if (response.headers.has('content-type') && response.headers.get('content-type').includes('application/json')) {
+            return response.json();
+        } else {
+            return response.text();
+        }
+
     } catch (error) {
-        console.error("Error fetching:", error.message);
+        alert(error.message);
         throw error;
     }
-
-    // try {
-
-    //     const response = await fetch(baseURL + url, option);
-        
-    //     if (response.ok !== true) {
-    //         const err = await response.json();
-    //         // throw new Error(err.message);
-    //     }
-
-    //     if (response.status === 204) {
-    //         return response;
-    //     } else if (response.headers.has('content-type') && response.headers.get('content-type').includes('application/json')) {
-    //         return response.json();
-    //     } else {
-    //         return response.text();
-    //     }
-
-    // } catch (error) {
-    //     alert(error.message);
-    //     throw error;
-    // }
 
 }
 
